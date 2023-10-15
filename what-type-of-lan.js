bfb5524a9e3c438e8731598df9b9c41b9b7bc59f
Mohamed Siraj <siraj@oganro.com>
2023-10-15 22:34:45 +0530

    what-type-of-lan

---
+++ what-type-of-lan.js
@@ -0,0 +1,18 @@
+/**
+ * Is javascript a statically typed or a dynamically typed language?
+ * 
+ * answer
+ * 
+ * javascript is an dynamic typed language 
+ */
+
+//example
+
+var a = 6;
+a = "mohamed siraj";
+
+console.log(a);
+
+/**
+ * first time a variable declared as an integer type variable also we passed string type of value with a variable
+ */
